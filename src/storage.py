bd0062541a4c06f517badb6b1bc98e23c1d28935
Lin Changqing <782185366@qq.com>
2025-12-14 10:59:31 +0800

    feat: reframe storage.py using class Storage to wrap load_data(), save_data(), add_project(), add_project(), add_session().

--- src/storage.py
+++ src/storage.py
@@ -2,46 +2,52 @@ import json
 import logging
 from src.config import data_path
 
-def load_data(path) -> tuple[dict, list]:
-    """
-    load data from json file
-    :return: projects dictionary，sessions list
-    """
-    try:
-        with open(path, 'r', encoding='utf-8') as f:
-            data = json.load(f)
-            projects = data['projects']
-            sessions = data['sessions']
-            logging.info("successfully load data")
-        return projects, sessions
-    except FileNotFoundError:
-        # file not found, maybe the first time or deleted, return empty data
-        logging.error("file not found")
-        return {}, []
-    except json.decoder.JSONDecodeError:
-        # illegal format
-        logging.error("json decode error")
-        return {}, []
-    except KeyError:
-        # the data file may be modified maliciously, don't use it
-        logging.error("key error")
-        return {}, []
+class Storage:
+    def __init__(self, path):
+        self.path = path
+        self.projects, self.sessions = self.load_data()
 
-def save_data(projects, sessions):
-    with open(data_path, 'w') as f:
-        data = {'projects': projects, 'sessions': sessions}
+    def load_data(self) -> tuple[dict, list]:
+        """
+        load data from json file
+        :return: projects dictionary，sessions list
+        """
         try:
-            json.dump(data, f)
+            with open(self.path, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                projects = data['projects']
+                sessions = data['sessions']
+                logging.info("successfully load data")
+            return projects, sessions
+        except FileNotFoundError:
+            # file not found, maybe the first time or deleted, return empty data
+            logging.error("file not found")
+            return {}, []
         except json.decoder.JSONDecodeError:
+            # illegal format
             logging.error("json decode error")
+            return {}, []
+        except KeyError:
+            # the data file may be modified maliciously, don't use it
+            logging.error("key error")
+            return {}, []
 
-def add_project(name, path):
-    projects, sessions = load_data(data_path)
-    # add a new project into data
-    projects[name] = path
-    save_data(projects, sessions)
 
-def add_session(session_dict):
-    projects, sessions = load_data(data_path)
-    sessions.append(session_dict)
-    save_data(projects,sessions)
+    def save_data(self):
+        with open(data_path, 'w') as f:
+            data = {'projects': self.projects, 'sessions': self.sessions}
+            try:
+                json.dump(data, f)
+            except json.decoder.JSONDecodeError:
+                logging.error("json decode error")
+
+
+    def add_project(self, name, path):
+        # add a new project into data
+        self.projects[name] = path
+        self.save_data()
+
+
+    def add_session(self, session_dict):
+        self.sessions.append(session_dict)
+        self.save_data()
--- test/test_storage.py
+++ test/test_storage.py
@@ -1,13 +1,17 @@
 from unittest import TestCase
-from src.storage import load_data
+from src.storage import Storage
 from pathlib import Path
 
 
 class Test(TestCase):
+    def SetUp(self):
+        self.storage = Storage(Path(__file__).parent / 'fixture')
     def test_load_data(self):
         # correct_data_path = r'C:\dev\01_workspace\devtracker\test\fixture\correct_data.json'
         correct_data_path = Path(__file__).parent / 'fixture' / 'correct_data.json'
-        projects, sessions = load_data(correct_data_path)
+        storage = Storage(correct_data_path)
+        projects = storage.projects
+        sessions = storage.sessions
         self.assertIsNotNone(projects)
         self.assertIsNotNone(sessions)
         self.assertIn('my-blog', projects)
@@ -23,3 +27,8 @@ class Test(TestCase):
             "note": "实现了文章列表页的React组件"
         }]
         self.assertEqual(expected_sessions, sessions)
+
+    def test_save_data(self):
+        save_data_path = Path(__file__).parent / 'fixture' / 'save_data.json'
+        projects = {"cli-demo": "/Users/student/code/cli-demo"}
+        sessions = [{"project": "cli-demo", "start_time": "2023-10-27T10:00:00", "duration_seconds": 3600, "note": ""}]
