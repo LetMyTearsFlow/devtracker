741bfe5e75b843dcddcb00e638ac5f220705f4d4
Lin Changqing <782185366@qq.com>
2025-12-17 10:23:24 +0800

    feat: add a unit test for scan_projects.

--- test/test_manager.py
+++ test/test_manager.py
@@ -11,3 +11,10 @@ class TestManager(TestCase):
                            Path(__file__).parent / 'fixture' / 'test_scan_directory' / 'folder2' / 'folder22',
                            Path(__file__).parent / 'fixture' / 'test_scan_directory' / 'folder3']
         self.assertEqual(result, expected_result)
+
+    def test_scan_projects(self):
+        manager = Manager()
+        root = (Path(__file__).parent / 'fixture' / 'test_scan_directory')
+        manager.scan_projects(root)
+        self.assertIn("folder2", manager.storage.projects)
+        self.assertIn("folder3", manager.storage.projects)
